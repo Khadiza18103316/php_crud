32773b4b41bae789b84477a18d3fd0b9be7da608
Khadiza <khadizaakhi756@gmail.com>
2022-05-10 19:59:25 +0600

    read data

--- insert.php
+++ insert.php
@@ -14,7 +14,7 @@ $connect =mysqli_connect("localhost","root", "", "php_crud");
       $phone=$_POST['phone'];
       $dept=$_POST['dept'];
 
-      $query = "INSERT INTO insert_data(name,email,password,phone,dept) VALUES ('$name','$email','$password','      $phone','$dept')";
+      $query = "INSERT INTO insert_data(name,email,password,phone,dept) VALUES ('$name','$email','$password','$phone','$dept')";
 
       $insert = mysqli_query($connect,$query);
 
@@ -37,24 +37,96 @@ $connect =mysqli_connect("localhost","root", "", "php_crud");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>insert</title>
+
+    <style>
+    .container {
+        width: 500px;
+        height: 400px;
+        margin: 0 auto;
+        text-align: center;
+    }
+
+    input {
+        border: 2px solid green;
+        width: 90%;
+        padding: 8px;
+    }
+
+    select {
+        border: 2px solid green;
+        width: 90%;
+        padding: 8px;
+    }
+
+    button {
+        background-color: blue;
+        width: 90%;
+        padding: 10px 20px;
+        color: white;
+        font-size: 1.2rem;
+    }
+
+    h1 {
+        background-color: blue;
+        padding: 10px;
+        color: white;
+        text-align: center;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid green;
+        border-collapse: collapse;
+    }
+    </style>
 </head>
 
 <body>
-    <form method="POST">
-        <input type="text" name="name" placeholder="Name"><br><br>
-        <input type="email" name="email" placeholder="Email"><br><br>
-        <input type="password" name="password" placeholder="Password"><br><br>
-        <input type="number" name="phone" placeholder="Phone"><br><br>
-        <select name="dept">
-            <option value="CSE">CSE</option>
-            <option value="EEE">EEE</option>
-            <option value="BBA">BBA</option>
-        </select>
-        <br><br>
-        <button name="send">Send</button>
+    <h1>Insert Data</h1>
+    <div class="container">
+        <form method="POST">
+            <input type="text" name="name" placeholder="Name"><br><br>
+            <input type="email" name="email" placeholder="Email"><br><br>
+            <input type="password" name="password" placeholder="Password"><br><br>
+            <input type="number" name="phone" placeholder="Phone"><br><br>
+            <select name="dept">
+                <option value="CSE">CSE</option>
+                <option value="EEE">EEE</option>
+                <option value="BBA">BBA</option>
+            </select>
+            <br><br>
+            <button name="send">Send</button>
+        </form>
+        <br>
+        <h1>Read Data</h1>
+        <table>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Phone</th>
+            <th>Dept</th>
 
-    </form>
+            <?php
+        $read = "SELECT * FROM insert_data";
+        $query = mysqli_query($connect, $read);
 
+        while($row = mysqli_fetch_array($query)){ ?>
+            <tr>
+                <td><?php echo $row["name"]; ?></td>
+                <td><?php echo $row["email"]; ?></td>
+                <td><?php echo $row["password"]; ?></td>
+                <td><?php echo $row["phone"]; ?></td>
+                <td><?php echo $row["dept"]; ?></td>
+            </tr>
+
+            <?php  }
+
+       ?>
+        </table>
+        <br>
+    </div>
+    <br>
 </body>
 
 </html>
