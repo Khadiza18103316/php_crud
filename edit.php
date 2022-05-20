9b1ec9b30fff701997f4a88d6ebc6ec297978896
Khadiza <khadizaakhi756@gmail.com>
2022-05-20 13:58:57 +0600

    update data

--- edit.php
+++ edit.php
@@ -1,31 +1,30 @@
 <?php
+
 $connect =mysqli_connect("localhost","root", "", "php_crud");
+$id =  $_GET['idNo'];
 
-// Check connection
-// if ($connect->connect_error) {
-//     die("Connection failed: " . $connect->connect_error);
-//   }
-//   echo "Connected successfully";
+$read = "SELECT * FROM insert_data WHERE Id = $id";
+$query = mysqli_query($connect, $read);
+$row = mysqli_fetch_array($query);
 
-  if(isset($_POST['send'])){
-      $name=$_POST['name'];
-      $email=$_POST['email'];
-      $password=$_POST['password'];
-      $phone=$_POST['phone'];
-      $dept=$_POST['dept'];
+if(isset($_POST['edit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $phone=$_POST['phone'];
+    $dept=$_POST['dept'];
 
-      $query = "INSERT INTO insert_data(name,email,password,phone,dept) VALUES ('$name','$email','$password','$phone','$dept')";
+    $update = "UPDATE insert_data SET name = '$name', email='$email', password = '$password', phone= '$phone', dept='$dept' WHERE Id = $id ";
 
-      $insert = mysqli_query($connect,$query);
+    $query = mysqli_query($connect, $update);
 
-      if($insert){
-          echo"<script>alert('Data send success')</script>";
+    if($query){
+        echo"<script>alert('Data Edit success')</script>";
       }
       else{
-        echo"<script>alert('Data send failed')</script>";
+        echo"<script>alert('Data Edit failed')</script>";
     }
-
-  }
+}
 
 ?>
 
@@ -36,7 +35,7 @@ $connect =mysqli_connect("localhost","root", "", "php_crud");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insert Page</title>
+    <title>Edit Page</title>
 
     <style>
     .container {
@@ -83,23 +82,23 @@ $connect =mysqli_connect("localhost","root", "", "php_crud");
 </head>
 
 <body>
-    <h2>Insert Data</h2>
+    <h2>Edit Data</h2>
     <div class="container">
         <form method="POST">
-            <input type="text" name="name" placeholder="Name"><br><br>
-            <input type="email" name="email" placeholder="Email"><br><br>
-            <input type="password" name="password" placeholder="Password"><br><br>
-            <input type="number" name="phone" placeholder="Phone"><br><br>
+            <input type="text" value="<?php echo $row['name']  ?>" name="name" placeholder="Name"><br><br>
+            <input type="email" value="<?php echo $row['email']  ?>" name="email" placeholder="Email"><br><br>
+            <input type="password" value="<?php echo $row['password']  ?>" name="password"
+                placeholder="Password"><br><br>
+            <input type="number" value="<?php echo $row['phone']  ?>" name="phone" placeholder="Phone"><br><br>
             <select name="dept">
                 <option value="CSE">CSE</option>
                 <option value="EEE">EEE</option>
                 <option value="BBA">BBA</option>
             </select>
             <br><br>
-            <button name="send">Send</button>
+            <button name="edit">Edit</button>
         </form>
         <br>
-        <h1>Read Data</h1>
         <table>
             <th>Id</th>
             <th>Name</th>
@@ -107,7 +106,6 @@ $connect =mysqli_connect("localhost","root", "", "php_crud");
             <th>Password</th>
             <th>Phone</th>
             <th>Dept</th>
-            <th>Edit</th>
 
             <?php
         $read = "SELECT * FROM insert_data";
@@ -121,14 +119,13 @@ $connect =mysqli_connect("localhost","root", "", "php_crud");
                 <td><?php echo $row["password"]; ?></td>
                 <td><?php echo $row["phone"]; ?></td>
                 <td><?php echo $row["dept"]; ?></td>
-                <td><a href="edit.php?idNo=<?php echo $row['id'] ?>">Edit</a></td>
             </tr>
             <?php  }
        ?>
         </table>
         <br>
+
     </div>
-    <br>
 </body>
 
 </html>
